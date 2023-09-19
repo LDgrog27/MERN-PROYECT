b8e989b5e2110e26fd9c06f6da63345144b21f6a
unknown <lautarolarrue@gmail.com>
2023-09-18 21:09:24 -0300

    login/logout

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from '../models/user.models.js'; // importamos el modelo de usuario
 import bcrypt from 'bcryptjs'; // importamos bcryptjs para encriptar la contraseña
-import {createAccesToken} from '../libs/jwt.js'
+import { createAccesToken } from '../libs/jwt.js'
 
 
 
@@ -18,24 +18,55 @@ export const register = async (req, res) => {
         });
 
         const userSaved = await newUser.save(); // save es un metodo de mongoose que nos permite guardar en la base de datos
-            // Es asincrono, por lo que debemos usar await
+        // Es asincrono, por lo que debemos usar await
         const token = await createAccesToken({ id: userSaved._id }); // usamos el metodo createAccesToken para crear un token de usuario
-        
-        res.cookie( 'token', token); // Cookie es propio de express para una respuesta
+
+        res.cookie('token', token); // Cookie es propio de express para una respuesta
         res.json({
-             id: userSaved._id,
-             username: userSaved.username,  // DATOS ENVIADOS AL FRONTEND
-             email: userSaved.email,
+            id: userSaved._id,
+            username: userSaved.username,  // DATOS ENVIADOS AL FRONTEND
+            email: userSaved.email,
         });
-       
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 
 };
 
+export const login = async (req, res) => {
+    const { email, password } = req.body
 
-export const login = (req, res) => { res.send('Login') }
+    try {
+
+        const userFound = await User.findOne({ email }); // Buscamos el usuario por el email
+        if (!userFound) return res.status(400).json({ message: 'User not found' }); // Si no existe el usuario, retornamos un mensaje de error
+        
+        const isMatch = await bcrypt.compare(password, userFound.password) // usamos bcrypt para comparar la contraseña ingresada
+
+        if (!isMatch) return res.status(400).json({ message: 'Incorrect password' }); // Si la contraseña es incorrecta, retornamos un mensaje de error
+
+
+        const token = await createAccesToken({ id: userFound._id }); // Cambio usuario guardado por usuario encontrado
+        res.cookie('token', token); // Cookie es propio de express para una respuesta
+        res.json({
+            id: userFound._id,
+            username: userFound.username,  // DATOS ENVIADOS AL FRONTEND
+            email: userFound.email,
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+};
+
+export const logout = (req, res) => { // Cookie queda basio y expira
+    res.cookie('token', '', { 
+        expires: new Date(0) // Fecha de expiracion
+     }); 
+     return res.sendStatus(200)
+}
 
 
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express' // importamos el metodo Router de express, pero no como servidor, sino para crear un enrutador
-import { register, login } from '../controllers/auth.controllers.js'
+import { register, login, logout } from '../controllers/auth.controllers.js'
 
 
 const router= Router() //creamos un objeto de tipo router que nos va a permitir definir rutas (POST, GET, PUT, DELETE)
@@ -8,5 +8,6 @@ router.post('/register', register) // Cuando haga una peticion a /register, va a
 
 router.post('/login', login) // Cuando haga peticion a /login, va a ejecutar la funcion login
 
+router.post('/logout', logout)
 
 export default router // exportamos el router para poder usarlo en otros archivos
